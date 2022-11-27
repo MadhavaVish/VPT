070737cfec58f0beb86b3ed653c07ae3ea88266f
Kobbot <kobbotdev@gmail.com>
2022-11-27 23:28:18 +0100

    Fixed Plane

--- VPT/src/Scene/Plane.hpp
+++ VPT/src/Scene/Plane.hpp
@@ -6,8 +6,8 @@
 class Plane : public Shape {
 public:
 
-	Plane(const Transform* t, glm::vec3 n, float d);
-	void Intersect(const int idx, Ray& ray) const;
+	Plane(glm::vec3 n, float d);
+	bool Intersect(const int idx, Ray& ray) const;
 	glm::vec3 GetNormal(const glm::vec3 I) const;
 	glm::vec3 GetAlbedo(const glm::vec3 I) const;
 
